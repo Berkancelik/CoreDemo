9fe1182d8b5b907d4f9bc267bb49e79c81dabf9a
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-03-24 12:19:02 +0300

    interfacces has been added

--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -17,5 +17,10 @@ namespace EntityLayer.Concrete
         public string BlogImage { get; set; }
         public string BlogCreateDate { get; set; }
         public bool BlogStatus { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public List<Comment> Comments { get; set; }
     }
 }
