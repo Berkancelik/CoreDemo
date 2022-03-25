9ace1691ddc914bcbe1720b9f6a0449fc3e300ef
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-03-25 13:05:33 +0300

    IBlogService, BlogManager, BlogController has been created and some fixed

--- DataAccessLayer/EntityFreamework/EfBlogRepository.cs
+++ DataAccessLayer/EntityFreamework/EfBlogRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFreamework
 {
-    class EfBlogRepository: GenericRepository<Blog>, IBlogDal
+    public class EfBlogRepository: GenericRepository<Blog>, IBlogDal
     {
     }
 }
