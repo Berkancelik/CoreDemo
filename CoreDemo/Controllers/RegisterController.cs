fa9c138319238e7e2abef9befd38d178909b8a07
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-03-26 15:06:30 +0300

    Registration will continue

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Ekleme işlemi yapılırken, httpget ve httppost attributelerinin tanımlandığı metotların isimleri aynı olmak zorundadır.
+        // Http Get --> SAyfa yüklenince
+        // HttpPost --> Sayfada buton tetiklenince
+
+        // HttpGet Attribute komutu sayfada ketegorize ya da benzeri işlemler kullanılırken, sayfa yüklendiği anda listelenmesi istenene niteliklerde kullanılabilir.
+
+
+        [HttpPost]
+        public IActionResult Degisecek()
+        {
+            return View();
+        }
+    }
+}
