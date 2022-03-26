16e4ce189200146b0d853cc3a06f5cd41a0b43cf
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-03-27 01:35:46 +0300

    Fluent Validation

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilmez");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilmez");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilmez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en fazla 50 karakterlik veri girişi yapın");
+
+
+        }
+    }
+}
