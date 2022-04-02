c1782d99218db39297c479759246385e21e9bd53
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-02 14:37:16 +0300

    Error Page has been created and completed

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -39,6 +39,10 @@ namespace CoreDemo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // durum kodlarýný sayfalarýný kullan adýnda bir metod tanýmlarýz
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
