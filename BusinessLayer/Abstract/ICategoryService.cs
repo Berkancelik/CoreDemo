4e521478605c9a0d7ceb5fdc918a22843c3c9794
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-04 12:52:49 +0300

    IGenericService hsa been added and Refactoring

--- BusinessLayer/Abstract/ICategoryService.cs
+++ BusinessLayer/Abstract/ICategoryService.cs
@@ -7,15 +7,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Abstract
 {
-    public interface ICategoryService
-    {
-        void CategoryAdd(Category category);
-        void CategoryDelete(Category category);
-        void CategoryUpdate(Category category);
-
-        List<Category> GetList();
-        Category GetById(int id);
-        
+    public interface ICategoryService:IGenericService<Category>
+    {      
         
     }
 }
--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -18,20 +18,7 @@ namespace BusinessLayer.Concrete
             _blogDal = blogDal;
         }
 
-        public void BlogAdd(Blog blog)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void BlogDelete(Blog blog)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void BlogUpdate(Blog blog)
-        {
-            throw new NotImplementedException();
-        }
+  
 
         public List<Blog> GetBlogListWithCategory()
         {
@@ -61,6 +48,21 @@ namespace BusinessLayer.Concrete
         public List<Blog> GetBlogListByWriter(int id)
         {
             return _blogDal.GetListAll(x => x.WriterID == id);
+        }
+
+        public void TAdd(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Blog t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Blog t)
+        {
+            throw new NotImplementedException();
         }
     }
 }
