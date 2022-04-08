b4800bbcfdafa3624029e096bdb5c0374ccbb62a
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-08 11:58:03 +0300

    BlogRayting class has been cerated and some database fixes

---
+++ DataAccessLayer/Migrations/20220408085038_mig_add_score_column.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_add_score_column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "MailStatus",
+                table: "NewsLetters",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mail",
+                table: "NewsLetters",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BlogScore",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlogScore",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MailStatus",
+                table: "NewsLetters",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Mail",
+                table: "NewsLetters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
