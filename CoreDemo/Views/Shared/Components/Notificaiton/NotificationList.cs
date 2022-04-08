1cfc859a1f12be0896ee7bbbe54d76d1d1448fd6
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-09 00:54:28 +0300

    Notification architecture has been completed

---
+++ CoreDemo/Views/Shared/Components/Notificaiton/NotificationList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Views.Shared.Components.Notificaiton
+{
+    public class NotificationList:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetLast3Blog();
+            return View(values);
+        }
+    }
+}
