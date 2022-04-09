215760f69c346d3689e43595f934864189d53de7
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-09 12:57:32 +0300

    Notification listed

--- EntityLayer/Concrete/Notification.cs
+++ EntityLayer/Concrete/Notification.cs
@@ -10,7 +10,7 @@ namespace EntityLayer.Concrete
     public class Notification
     {
         [Key]
-        public int NotificationId { get; set; }
+        public int NotificationID { get; set; }
         public string NotificationType { get; set; }
         public string NotificationTypeSymbol { get; set; }
         public string NotificationDetails { get; set; }
