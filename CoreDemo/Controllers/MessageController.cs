2291bee436d6c690f70bdde21dedc5b77780072d
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-12 15:34:38 +0300

    fixed

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+
+        public IActionResult InBox()
+        {
+            int id = 1;
+            var values = mm.GetInboxListByWriter(id);
+            return View(values);
+        }
+    }
+}
