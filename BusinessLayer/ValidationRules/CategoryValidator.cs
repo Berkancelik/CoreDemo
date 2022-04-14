d85d60fb290e46f127a567193da33fbc1b746038
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-14 23:48:51 +0300

    Category Page and New Category has been completed.

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("Kategori adını en fazla 50 karakter olmalıdır.");
+            RuleFor(x => x.CategoryName).MaximumLength(5).WithMessage("Kategori adını en az 5 karakter olmalıdır.");
+        }
+    }
+}
--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
 ﻿
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,6 +16,7 @@ namespace CoreDemo.Areas.Admin.Controllers
 {
     // Bu areaların adminden geldiğini bildirmiş oluruz. 
     [Area("Admin")]
+    [AllowAnonymous]
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
@@ -20,6 +25,34 @@ namespace CoreDemo.Areas.Admin.Controllers
             // PAged list de başlangıç ikinci ise pagesize parametredir. yani her sayfada kaç değer olacak
             var values = cm.GetList().ToPagedList(page, 3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        } 
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            {        
+
+                CategoryValidator cv = new CategoryValidator();
+                ValidationResult results = cv.Validate(p);
+                if (results.IsValid)
+                {
+                    p.CategoryStatus = true;
+                    cm.TAdd(p);
+                    return RedirectToAction("Index", "Category");
+                }
+                else
+                {
+                    foreach (var item in results.Errors)
+                    {
+                        ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    }
+                }
+                return View();
+            }
         }
     }
 }
