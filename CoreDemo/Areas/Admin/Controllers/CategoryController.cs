c03171733a9f008ce551ac551d365d2604b6991b
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-14 23:20:52 +0300

    PagedList Has been completed

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -14,9 +15,10 @@ namespace CoreDemo.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var values = cm.GetList();
+            // PAged list de başlangıç ikinci ise pagesize parametredir. yani her sayfada kaç değer olacak
+            var values = cm.GetList().ToPagedList(page, 3);
             return View(values);
         }
     }
