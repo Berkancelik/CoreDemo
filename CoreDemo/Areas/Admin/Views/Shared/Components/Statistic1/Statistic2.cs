27d1c545d7e92175b7aedf3594965eaa65100ce0
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-19 23:10:32 +0300

    Statistics2 Has been completed

---
+++ CoreDemo/Areas/Admin/Views/Shared/Components/Statistic1/Statistic2.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Views.Shared.Components.Statistic1
+{
+    public class Statistic2:ViewComponent
+    {
+
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x=>x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
