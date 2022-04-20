e079ba57e19333be47174c1f9a7ee45692f27f3f
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-20 17:04:14 +0300

    Static Chart complete but error occurred

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [AllowAnonymous]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 10
+            });  list.Add(new CategoryClass
+            {
+                categoryname = "Spor",
+                categorycount = 14
+            });  list.Add(new CategoryClass
+            {
+                categoryname = "Yazılım",
+                categorycount = 5
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
