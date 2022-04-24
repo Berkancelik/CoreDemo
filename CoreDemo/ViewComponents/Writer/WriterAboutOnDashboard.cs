335e4df84db659dc104e0d84912d69b1ec16f144
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-25 01:02:55 +0300

    Login Has been completed

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,14 +11,20 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.ViewComponents.Writer
 {
-    public class WriterAboutOnDashboard:ViewComponent
+    public class WriterAboutOnDashboard : ViewComponent
     {
         WriterManager writermanager = new WriterManager(new EfWriterRepository());
         Context c = new Context();
 
+   
+
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
+            var username = User.Identity.Name;
+            ViewBag.v =  username = User.Identity.Name;
+
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            ;
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = writermanager.GetWriterByID(writerID);
             return View(values);
