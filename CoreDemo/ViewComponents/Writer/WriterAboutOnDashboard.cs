1951f23acbcd417708461ca5acdad4e6ca295a2e
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-25 13:58:42 +0300

    Refactoring

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -16,15 +16,13 @@ namespace CoreDemo.ViewComponents.Writer
         WriterManager writermanager = new WriterManager(new EfWriterRepository());
         Context c = new Context();
 
-   
+
 
         public IViewComponentResult Invoke()
         {
             var username = User.Identity.Name;
-            ViewBag.v =  username = User.Identity.Name;
-
+            ViewBag.v = username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
-            ;
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = writermanager.GetWriterByID(writerID);
             return View(values);
