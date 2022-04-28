cbcd86692fd76ee29668dee5813a3511ac555424
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-04-28 11:30:09 +0300

    Completion of message processing

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFreamework;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,14 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterMessageNotification: ViewComponent
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            int id = 1;
-            var values = mm.GetInboxListByWriter(id);
+
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
             return View(values);
         }
 
--- DataAccessLayer/EntityFreamework/EfMessage2Repository.cs
+++ DataAccessLayer/EntityFreamework/EfMessage2Repository.cs
@@ -13,12 +13,23 @@ namespace DataAccessLayer.EntityFreamework
 {
     public class EfMessage2Repository : GenericRepository<Message2>, IMessage2Dal
     {
-        public List<Message2> GetListWithMessageByWriter(int id)
+        public List<Message2> GetSendboxWithMessageByWriter(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Message2s.Include(x => x.ReceiverUser).Where(y => y.ReceiverID == id).ToList();
+            }
+        }
+
+      
+        public List<Message2> GetInboxWithMessageByWriter(int id)
         {
             using (var c = new Context())
             {
                 return c.Message2s.Include(x => x.SenderUser).Where(x => x.ReceiverID == id).ToList();
             }
         }
+
+   
     }
 }
